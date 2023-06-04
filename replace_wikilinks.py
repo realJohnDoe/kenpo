0ebf893112da42bee3fd2bd355c92504b63cd497
Johannes Döllinger <johannes.doellinger@gmail.com>
2023-06-04 12:40:03 +0200

    Update script

--- replace_wikilinks.py
+++ replace_wikilinks.py
@@ -2,40 +2,60 @@ import os
 import re
 
 
-def replace_wikilinks(file_path):
-    with open(file_path, "r") as file:
-        content = file.read()
-
-    # Replace wikilinks with markdown links
-    content = re.sub(r"\[\[(.*?)\]\]", replace_wikilink_with_header(file_path), content)
-
-    with open(file_path, "w") as file:
-        file.write(content)
-
-
-def replace_wikilink_with_header(file_path):
+def replace_wikilink_with_header(file_paths):
     def replace(match):
         target_file = match.group(1)
-        target_file_path = os.path.join(os.path.dirname(file_path), target_file + ".md")
+        print(f"Replacing wikilink targeting {target_file}.")
+        target_file_path_candidates = [
+            f for f in file_paths if f.endswith(target_file + ".md")
+        ]
+        if not target_file_path_candidates:
+            print("Did not find a matching file")
+            return target_file
+
+        target_file_path = target_file_path_candidates[0]
+        print(f"Found target file under {target_file_path}")
         with open(target_file_path, "r") as target_file:
             target_content = target_file.read()
             header_match = re.search(r"^#+ (.+)$", target_content, re.MULTILINE)
             if header_match:
-                return header_match.group(1)
+                title = header_match.group(1)
+                print(f"Found header {title}")
+                return title
             else:
+                print("Found no header. Using the file name instead.")
                 return target_file
 
     return replace
 
 
+def replace_wikilinks(file_paths):
+    for file_path in file_paths:
+        print(f"Replacing wikilinks in: {file_path}")
+        with open(file_path, "r") as file:
+            content = file.read()
+
+        # Replace wikilinks with markdown links
+        content = re.sub(
+            r"\[\[(.*?)\]\]", replace_wikilink_with_header(file_paths), content
+        )
+
+        with open(file_path, "w") as file:
+            file.write(content)
+
+
 # Replace wikilinks in all markdown files in a directory
 def replace_all_wikilinks(directory):
+    md_files = []
     for subdir, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".md"):
                 file_path = os.path.join(subdir, file)
-                replace_wikilinks(file_path)
+                md_files.append(file_path)
+    replace_wikilinks(md_files)
 
 
 # Example usage
-replace_all_wikilinks("/docs")
+target_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "docs"))
+print(target_dir)
+replace_all_wikilinks(target_dir)
